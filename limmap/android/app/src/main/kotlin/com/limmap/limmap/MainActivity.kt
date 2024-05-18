6cc4d6947295e4f1272325ecaefef46d0df3b69f
Boubakar PI <agenceweb1919@gmail.com>
2024-05-18 14:09:48 +0000

    18 mai 2024, 14:09

--- limmap/android/app/src/main/kotlin/com/limmap/limmap/MainActivity.kt
+++ limmap/android/app/src/main/kotlin/com/limmap/limmap/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.limmap.limmap
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
